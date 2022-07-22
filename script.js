2292581d9567fa570a7e35d9e7ab32faf4f63e2a
Casey.Borrow <Caseyborrow@gmail.com>
2022-07-22 22:56:27 +0100

    Create starting grid to use as a canvas. Uses a function which will allow, in the future, the user to choose own size of grid.

---
+++ script.js
@@ -0,0 +1,31 @@
+var grid = document.getElementById("gridContainer");
+
+function createRow() {
+    var row = document.createElement("div");
+    row.className = "gridRow";
+}
+
+function createSquare() {
+    var square = document.createElement("div");
+    square.className = "gridSquare"
+}
+
+function createGrid(x) {
+    deleteGrid();
+    for (let i = 0; i < x; i++) {
+        var row = document.createElement("div");
+        row.className = "gridRow";
+        for (let y = 0; y < x; y++) {
+            var square = document.createElement("div");
+            square.className = "gridSquare"
+            row.appendChild(square);
+        }
+        grid.appendChild(row)
+    }
+}
+
+function deleteGrid() {
+    while (grid.firstChild) grid.removeChild(grid.firstChild);
+}
+
+createGrid(16)
